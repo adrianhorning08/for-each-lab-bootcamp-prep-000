089ed6705719417f37cdfb9887ba90d4c6843877
Adrian Horning <m.adrian.horning@gmail.com>
2017-03-21 19:01:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function iterativeLog(array) {
-  array.forEach(console.log("${index}: ${element}"))
+  array.forEach(function(index, element) {
+    console.log(`${element}: ${index}`);
+  }
+)}
+
+function iterate(callback) {
+  var myArray = ['chocolate', 'vanilla', 'strawberry']
+  myArray.forEach(callback)
+  return myArray
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
